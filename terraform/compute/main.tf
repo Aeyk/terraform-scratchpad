ec4ee8eca81b59a7d1bc2baea87630ab4c27ed73
mksybr@gmail.com <mksybr@gmail.com>
2024-01-10 23:33:46 -0500

    bugfix dependency

--- terraform/compute/main.tf
+++ terraform/compute/main.tf
@@ -261,4 +261,10 @@ resource "digitalocean_record" "amd-1vcpu-1gb-us-qas-a-dns-record" {
 #   ttl = "30"
 # }
 
-
+resource "local_file" "ansible_inventory" {
+  content = templatefile("../ansible/inventory.ini.tmpl", {
+    # amd-1vcpu-1gb-us-qas-public_ipv4 = oci_core_instance.amd-1vcpu-1gb-us-qas.*.public_ip
+    arm-1vcpu-6gb-us-qas-public_ipv4 = oci_core_instance.arm-1vcpu-6gb-us-qas.*.public_ip
+  })
+  filename = "../ansible/inventory.ini"
+}
--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ubuntu_public_ips" {
-  value = [for u in oci_core_instance.amd-1vcpu-1gb-us-qas : u.public_ip[*]]
-}
-
-output "arm_public_ips" {
-  value = [for u in oci_core_instance.arm-1vcpu-6gb-us-qas : u.public_ip[*]]
-}
