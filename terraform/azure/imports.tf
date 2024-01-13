f0f5252b512e2a6f255d9f2cff52dad902115e5e
mksybr@gmail.com <mksybr@gmail.com>
2024-01-13 07:23:12 -0500

    azure

--- terraform/azure/imports.tf
+++ terraform/azure/imports.tf
@@ -1,4 +1,4 @@
 # import {
-#   id = "/subscriptions/4c604c25-175c-4a04-8d89-89986398b1d5/resourceGroups/azure_k8s_eastus2"
+#   id = "/subscriptions/4c604c25-175c-4a04-8d89-89986398b1d5/resourceGroups/azure_resource_group"
 #   to = module.meta.azurerm_resource_group.main
 # }
--- terraform/azure/secrets/outputs.tf
+++ terraform/azure/secrets/outputs.tf
@@ -32,21 +32,21 @@ output "home_ssh_key" {
 
 # resource "azurerm_ssh_public_key" "phone_ssh_key" {
 #   name                = "phone_ssh_key"
-#   resource_group_name = "azure_k8s_eastus2"
+#   resource_group_name = "azure_resource_group"
 #   location            = "eastus"
 #   public_key          = data.keepass_entry.phone_public_ssh_key_contents.attributes.public_key
 # }
 
 resource "azurerm_ssh_public_key" "home_ssh_key" {
   name                = "home_ssh_key"
-  resource_group_name = "azure_k8s_eastus2"
+  resource_group_name = "azure_resource_group"
   location            = "eastus2"
   public_key          = file(var.public_ssh_key)
 }
 
 resource "azurerm_ssh_public_key" "work_ssh_key" {
   name                = "work_ssh_key"
-  resource_group_name = "azure_k8s_eastus2"
+  resource_group_name = "azure_resource_group"
   location            = "eastus2"
   public_key          = file(var.work_public_ssh_key)
 }
