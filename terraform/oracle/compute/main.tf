467f019e12054e6bae83cdc872c825ce9751d0ea
Malik Kennedy <mksybr@gmail.com>
2024-02-02 08:45:29 -0500

    feat/wip: single arm instance left

--- terraform/oracle/compute/main.tf
+++ terraform/oracle/compute/main.tf
@@ -253,7 +253,7 @@ resource "digitalocean_record" "amd-1vcpu-1gb-us-qas-a-dns-record" {
 # }
 
 resource "local_file" "ansible_inventory" {
-  content = templatefile("../ansible/inventory.ini.tmpl", {
+  content = templatefile("../../ansible/inventory.ini.tmpl", {
     # amd-1vcpu-1gb-us-qas-public_ipv4 = oci_core_instance.amd-1vcpu-1gb-us-qas.*.public_ip
     arm-1vcpu-6gb-us-qas-public_ipv4 = oci_core_instance.arm-1vcpu-6gb-us-qas.*.public_ip
   })
