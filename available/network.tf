7142c652908fd6724dd5944f22a739bb5e5b27d3
Malik Kennedy <mksybr@gmail.com>
2024-02-18 03:52:13 -0500

    enable management + wip ipv6

--- available/network.tf
+++ available/network.tf
@@ -58,6 +58,19 @@ resource "oci_core_route_table" "igw_route_table" {
   }
 }
 
+resource "oci_core_route_table" "igw_ipv6_route_table" {
+  depends_on = [oci_core_vcn.vcn, oci_core_internet_gateway.igw]
+  compartment_id = data.keepass_entry.oci_compartment_id.password
+  vcn_id         = oci_core_vcn.vcn.id
+  display_name   = "cloud-mksybr-igw-ipv6-route-table"
+  
+  route_rules {
+    destination      = "::/0"
+    destination_type = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.igw.id
+  }
+}
+
 # resource "oci_core_nat_gateway" "nat_gateway" {
 #   depends_on = [oci_core_vcn.vcn]
 #   compartment_id = data.keepass_entry.oci_compartment_id.password
