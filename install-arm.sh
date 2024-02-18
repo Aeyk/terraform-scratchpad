b36cc1e234099fcfe986a07fccedb20f4016342a
Malik Kennedy <mksybr@gmail.com>
2024-02-18 03:52:11 -0500

    no open firewall rule because an ssh tunnel is good enough

--- install-arm.sh
+++ install-arm.sh
@@ -10,7 +10,10 @@ dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarc
 dnf config-manager --set-enabled crb
 dnf install -y epel-release epel-release-next
 dnf install -y --enablerepo="epel" xrdp
-firewall-cmd --permanent --add-service=rdp
-firewall-cmd --permanent --add-port=3389/tcp
-firewall-cmd --reload
+# not necessary as i will make a ssh tunnel on client before connecting via:
+# 	ssh opc@me.mksybr.com -L3389:127.0.0.1:3389
+# firewall-cmd --permanent --add-service=rdp
+# firewall-cmd --permanent --add-port=3389/tcp
+# firewall-cmd --reload
+echo 'me  ALL=(ALL:ALL) ALL' >> /etc/sudoers.d/me
 systemctl enable --now xrdp
