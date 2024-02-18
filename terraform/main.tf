e0f2f82ba8f971a22f1f733e0e12f9ac3ace853d
Malik Kennedy <mksybr@gmail.com>
2024-02-18 03:52:14 -0500

    extract providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,20 @@ provider "aws" {
     access_key = data.keepass_entry.aws_access_key.password
     secret_key = data.keepass_entry.aws_secret_key.password
 }
+
+provider "keepass" {
+  database = "../Cloud Tokens.kdbx"
+  password = var.database_password
+}
+
+provider "digitalocean" {
+  token = data.keepass_entry.digitalocean_token.password
+}
+
+provider "oci" {
+  tenancy_ocid = data.keepass_entry.oci_tenancy_id.password
+  user_ocid = data.keepass_entry.oci_user_id.password
+  private_key_path = "/home/malik/.oci/mksybr@gmail.com_2023-12-24T00_16_14.614Z.pem"
+  fingerprint = data.keepass_entry.oci_fingerprint.password
+  region = "us-ashburn-1"
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,18 +20,26 @@ locals {
   }
 }
 
+variable "database_password" { 
+ sensitive = true
+}
+
 data "keepass_entry" "digitalocean_token" {
   path = "Root/DigitalOcean Token"
 }
+
 data "keepass_entry" "oci_fingerprint" {
   path = "Root/Oracle OCI fingerprint"
 }
+
 data "keepass_entry" "oci_tenancy_id" {
   path = "Root/Oracle Tenancy ID"
 }
+
 data "keepass_entry" "oci_compartment_id" {
   path = "Root/Oracle Compartment ID"
 }
+
 data "keepass_entry" "oci_user_id" {
   path = "Root/Oracle User ID"
 }
