fbb3ceca43eaea52e49a435455ce2a004c515480
Malik Kennedy <mksybr@gmail.com>
2024-02-18 03:52:14 -0500

    bugfix dependency

--- terraform/compute/outputs.tf
+++ terraform/compute/outputs.tf
@@ -1,3 +1,11 @@
+output "ubuntu_public_ips" {
+  value = [for u in oci_core_instance.amd-1vcpu-1gb-us-qas : u.public_ip[*]]
+}
+
+output "arm_public_ips" {
+  value = [for u in oci_core_instance.arm-1vcpu-6gb-us-qas : u.public_ip[*]]
+}
+
 output "all-availability-domains-in-your-tenancy" {
   value = data.oci_identity_availability_domains.ads.availability_domains
 }
