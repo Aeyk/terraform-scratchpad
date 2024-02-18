5769127ac1faa0d9b6b5b93a978f1367f6b3e2b9
Malik Kennedy <mksybr@gmail.com>
2024-02-18 03:52:12 -0500

    refactor: extract out versions

---
+++ available/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_version = "~> 1.6"
+  required_providers {
+    oci = {     
+      source  = "oracle/oci"
+      version = "~> 5"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2"
+    }
+    keepass = {
+      source = "iSchluff/keepass"
+      version = "~> 0"
+    }
+  }
+}
