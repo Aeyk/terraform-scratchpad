4c1cba563f21bce706fa1e145a006956aecf560a
Malik Kennedy <mksybr@gmail.com>
2024-03-11 00:18:00 +0000

    bugfix: metallb needs cidr not fqdn

--- terraform/oracle/compute/main.tf
+++ terraform/oracle/compute/main.tf
@@ -282,6 +282,15 @@ resource "local_file" "ansible_inventory" {
   filename = "../../ansible/inventory.ini"
 }
 
+resource "local_file" "metallb_address_pool" {
+  content = templatefile("./templates/metallb-ip-address-pool.yml", {
+    # amd-1vcpu-1gb-us-qas-public_ipv4 = oci_core_instance.amd-1vcpu-1gb-us-qas.*.public_ip
+    arm-1vcpu-6gb-us-qas-public_ipv4 = oci_core_instance.arm-1vcpu-6gb-us-qas.*.public_ip
+  })
+  filename = "../../kubernetes/metallb/ip-address-pool.yml"
+}
+
+
 resource "local_file" "provision-script" {
   depends_on = [oci_core_instance.arm-1vcpu-6gb-us-qas]
   content = templatefile("./templates/provision-cluster.sh.tmpl", {
